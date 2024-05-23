0b1deefafd3cb53c27e2426e2827139f5457d58e
Sonawane <pradeep.sonawane@siemens.com>
2024-05-23 23:57:55 +0530

    video url added

--- src/app/video-player/video-player.component.ts
+++ src/app/video-player/video-player.component.ts
@@ -1,7 +1,7 @@
 import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { SearchService } from '../search-doc/services/search.service';
-import { DomSanitizer, SafeUrl } from '@angular/platform-browser';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 import {
   CustomInsightsWidget,
   IBaseStyleConfig,
@@ -16,6 +16,8 @@ import {
   InsightsWidget,
   IWidgetOptions
 } from '@azure/video-indexer-widgets';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'video-player',
@@ -24,7 +26,6 @@ import {
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class VideoPlayerComponent implements OnInit {
-  public insightsWidget: InsightsWidget = {} as InsightsWidget;
   accountId: string = '';
   videoId: string = '';
   videoUrl: SafeUrl = '';
@@ -44,45 +45,22 @@ export class VideoPlayerComponent implements OnInit {
       this.accountId = params.get('accountId') ?? '' ;
       this.videoId = params.get('videoId') ?? '';
       this.startTime = decodeURIComponent(params.get('startTime') ?? '');
-      this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.getVideoUrl(this.startTime));
-      console.log('videoUrl:', this.videoUrl);
-      this.insightsUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.getInsightsUrl());
+      //this.videoUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.getVideoUrl(this.startTime));
+      //this.insightsUrl = this.domSanitizer.bypassSecurityTrustResourceUrl(this.getInsightsUrl());
     });
   }
 
-  public ngAfterViewInit(): void {
-    const insightsStyleConfig2: IBaseStyleConfig = {
-        primary: 'yellow',
-        dividers: 'rgba(134,28,173,1)'
-    };
-
-    const style: IWidgetStyle = {
-        customStyle: insightsStyleConfig2,
-        theme: 'Dark'
-    };
-
-    const config: IInsightsWidgetConfig = {
-        accountId: this.accountId,
-        videoId: this.videoId,
-        accessToken: this.accessToken,
-        locale: 'en-us',
-        tab: 'timeline',
-        components: ['transcript', 'keywords'],
-        style: style
-    };
-
-    const widgetOptions: IWidgetOptions = {
-        height: 780,
-        width: '100%'
-    };
-
-    this.insightsWidget = new InsightsWidget('container', widgetOptions, config);
-    this.insightsWidget.render();
-  }
-
-  getVideoUrl(startTime: string): string {
-    this.accessToken = this.searchService.videoToken;
-    return `https://www.videoindexer.ai/embed/player/${this.accountId}/${this.videoId}/?accessToken=${this.accessToken}&locale=en&location=trial?t=${this.getNoOfSecs(startTime)}`;
+  getVideoUrl(startTime: string): Observable<SafeResourceUrl> {
+    return this.searchService
+      .getVideoToken()
+      .pipe(
+        map((token: any) => {
+          this.accessToken = token;
+          const videoUrl = `https://www.videoindexer.ai/embed/player/${this.accountId}/${this.videoId}/?accessToken=${this.accessToken}&locale=en&location=trial?t=${this.getNoOfSecs(startTime)}`;
+          const videoUrlSafe = this.domSanitizer.bypassSecurityTrustResourceUrl(videoUrl);
+          this.videoUrl = videoUrlSafe;
+          return videoUrlSafe;
+        }));
   }
 
   getNoOfSecs(startTime: string): number {
@@ -99,9 +77,5 @@ export class VideoPlayerComponent implements OnInit {
 
   getInsightsUrl(): string {
     return `https://www.videoindexer.ai/embed/insights/${this.accountId}/${this.videoId}/?accessToken=${this.accessToken}&locale=en&location=trial`;
-  }
-
-  public get apiVersion() {
-    return this.insightsWidget?.apiVersion;
   }
 }
